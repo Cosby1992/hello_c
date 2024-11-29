eb0a96889dfc329a2a74c214abc2182d79f151aa
cosby1992 <andersbirkedahl@hotmail.com>
2024-11-29 22:13:42 +0100

    feat(datatypes): added first scanner

---
+++ datatypes/datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    // get integer inputs from user
+    // using scanf to take input from standard input (keyboard)
+    // scanf
+    scanf("Enter an integer: %d");
+
+    return 0;
+}
